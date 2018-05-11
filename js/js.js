9dbcb5b8e9e57b59301134581577136843dd3448
xScottishKrissx <chrisdunne66@gmail.com>
2018-05-11 13:54:29 +0100

    Day 2 - Pre Lunch
    Added Image Gallery
    Added Suggestions Carousel using slick plugin

--- js/js.js
+++ js/js.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
+  $('.product-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
 
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+      }
+    },
+    {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+      }
+    }
+    ]
+
+      //breakpoints
+      //default - 2 products shown
+      //576px - 3 products
+      //768px - 4 products
+  });
 });
